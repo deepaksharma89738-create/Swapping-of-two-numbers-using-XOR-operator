4d87e48bfe6f5ed83b4e80891f0fb73abbfb25fa
Deepak Sharma <deepaksharma89738@gmail.com>
2026-03-02 13:09:42 +0530

    Add files via upload

---
+++ Swapping of two numbers using XOR operator.C
@@ -0,0 +1,14 @@
+/*Swapping of two numbers using XOR operator*/
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
+}
